a76bbd66e53d8bc1709bca4aa6a372daf598bb43
shruti-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-06 09:47:01 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//equality check and comparisons wor differently. comparisons convert null to a number, treating it as 0.
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
