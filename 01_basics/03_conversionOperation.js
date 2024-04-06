b1c32ebc0945d2d23dffe15c3cf84127f9ecbc6a
shruti-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-06 06:58:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+// let score = null
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33 
+// "33abc" => NaN (but its type shows as number)
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "shruti" => true
