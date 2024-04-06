91d3ddbf291da9fe1870bc478aa0b7c791a26fb3
shruti-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-06 09:57:26 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,16 @@
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId);
+
+const bigNumber = 354658697078463577637666663733n
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "perman", "pikachu"]
+
+console.log(typeof bigNumber);
