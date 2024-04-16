de41f260ff07521d665cf6b8da97148d4684df0f
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-16 13:20:09 +0000

    just strted arrays video

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,12 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+
+// we do shallow copy in arrays
+// shallow copy shares same reference point (value gets change)
+
+//ways of declaring arrays
+
+const myArr2 = [2,3,4,5]
+
+const myArr3 = new Array()
