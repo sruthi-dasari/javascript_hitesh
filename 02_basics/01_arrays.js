18789815a0ba1ed73c6f930725eacf596ecf8011
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-17 10:54:48 +0000

    done with arrays part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -9,4 +9,34 @@ const myArr = [0,1,2,3,4,5]
 
 const myArr2 = [2,3,4,5]
 
-const myArr3 = new Array()
+const myArr3 = new Array(1,2,3,4)
+
+// console.log(myArr3)
+
+
+// array methods
+
+// myArr.unshift(9) // adds value at start
+// myArr.shift() // removes value from start
+// console.log(myArr)
+
+// console.log(myArr.includes(12)); 
+// console.log(myArr.indexOf(18)); //if value is not present it prints -1
+
+
+newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr); // join binds two arrays also converts it into string
+
+//  diff between slice and splice
+
+// const myArr = [0,1,2,3,4,5]
+
+console.log("A", myArr);
+myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B", myArr);
+myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C", myArr); // splice not only includes last range but also changes original array
