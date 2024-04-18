44c02c2fd75f37ff2c22c8d78b80cf81941e1b03
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-18 05:15:47 +0000

    finished objects part 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,19 +2,41 @@
 // Object.create
 
 //Object literals
+
+const mySym = Symbol("key1")
+
 const JSUser = {
     name: "Shruti",
     age: 27,
     "full name": "shruti dasari",
+    [mySym]: "mykey1", // we should declare symbols like this
     location: "Bilaspur",
     email: "shruti@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Tuesday"]
 }
 
-console.log(JSUser.email); 
-console.log(JSUser["full name"]); //array notation can be used to access keys which have space
+// console.log(JSUser.email); 
+// console.log(JSUser["full name"]); //array notation can be used to access keys which have space
+// console.log(typeof JSUser[mySym]); // to access symbols
 
+// we can freeze objects so that values cannot be changed
 
+// Object.freeze(JSUser)
+JSUser.email = "shruti@micro.com"
+// console.log(JSUser);
 
+// how to add functions in our object
 
+JSUser.greeting = function(){
+    console.log("Hello js user!");
+}
+
+JSUser.greetingTwo = function(){
+    console.log(`Hello js ${this.name}!`); // we can add values inside string using this syntax (string interpolation)
+    // we can use "this" here to refer keys of same object 
+}
+
+// console.log(JSUser.greeting); //here we are printing only function reference
+JSUser.greeting(); //here we are printing function definition
+JSUser.greetingTwo();
