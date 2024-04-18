27235a58e1a59d803d9ad1dd5249c4c0acc7b1c6
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-18 04:16:17 +0000

    finished arrays part 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,33 @@
+// ways of combining arrays
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros); // push modifies existing array
+// console.log(marvel_heros[3][0]);
+
+// const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros); // concat returns new array
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros] // using spread operator
+// console.log(all_new_heros);
+
+const anotherArr = [1,2,[3,4,[7,8,[3,4]]]]
+
+// const real_another_arr = anotherArr.flat(1)
+// const real_another_arr = anotherArr.flat(Infinity)
+// console.log(real_another_arr);
+
+// console.log(Array.isArray("Shruti"));
+// console.log(Array.from("Shruti"));
+
+// console.log(Array.from({name:"Shruti"})); // returns empty array because u need
+                                          //to define what u want to make array
+
+let score1 = 100  
+let score2 = 200                                          
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); 
