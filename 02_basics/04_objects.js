282db15ca893502469e2ddc9b2bd8289088e8014
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-18 07:33:08 +0000

    finished with objects part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,3 +1,42 @@
 // const tinderUser = new Object() // singleton object
-const tinderUser = {} // non-singleton object
+// const tinderUser = {} // non-singleton object
+// console.log(tinderUser);
+
+const tinderUser = {
+    id: "abc123",
+    name: "shruti",
+    isLoggedIn: false
+}
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"shruti",
+            lastname: "dasari"
+        }
+    }
+}
+
+
+// console.log(regularUser.fullname.userfullname);
+
+// ways of combining objects
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1,obj2}
+
+const obj3 = Object.assign({}, obj1, obj2) // empty {} is here target object
+                                           // if {} not given then first parameter is treated as target  
+// console.log(obj3);
+
 console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // here we get keys and values in arrays
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
