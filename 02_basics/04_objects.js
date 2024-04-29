2b2fd5e4af1dd20d6f1c94519228875209edd2c5
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-29 10:42:33 +0000

    json talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,11 +32,36 @@ const obj3 = Object.assign({}, obj1, obj2) // empty {} is here target object
                                            // if {} not given then first parameter is treated as target  
 // console.log(obj3);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser)); // here we get keys and values in arrays
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+// object destructuring video notes starts here
 
 
+course = {
+    coursename: "js in hindi",
+    courseInstructor: "hitesh",
+    price: 999
+}
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+const navbar = ({company}) =>{
+
+}
+
+navbar(company = "hitesh")
+
+// json api
+
+// {
+//     "name": "hitesh",
+//     "coursename": "json in hindi",
+//     "price": "free"
+// }
