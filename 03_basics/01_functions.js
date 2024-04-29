f9a9718bb0ba590097951c95748c339697632922
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-04-29 11:43:32 +0000

    done with functions with objs and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,12 +18,47 @@ function sayMyName(){ //function declaration
 
 // addTwoNumbers(1,2) // function arguments
 
-function addTwoNumbers(number1, number2){ //function parameters
-    return(number1 + number2);
+// function addTwoNumbers(number1, number2){ //function parameters
+//     return(number1 + number2);
 
-    console.log("shruti"); //whatever u write after return it will not execute
+//     console.log("shruti"); //whatever u write after return it will not execute
+// }
+
+// const result = addTwoNumbers(1,2)
+
+// console.log("Result:", result);
+
+//functions with objects and arrays
+
+// function calculateCartPrice(...num1){ //rest operator
+//     return num1
+// }
+
+// console.log(calculateCartPrice(10,20,30));  
+
+const user = {
+    username: "shruti",
+    price: "199"
 }
 
-const result = addTwoNumbers(1,2)
+function handleAnyObject(anyObject){
+    console.log(`Username is ${anyObject.username} price is ${anyObject.price}`);
+}
 
-console.log("Result:", result);
+// handleAnyObject(user)
+
+// directly passing object
+// handleAnyObject({
+//     username: "sam",
+//     price:"200"
+// })
+
+const newArr = [50,30,20,60]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(newArr));
+
+console.log(returnSecondValue([20,100,3000]));
