24f3ad4b44262319eb00d4ec5ff1b3ee1e1209f8
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-05-01 11:09:50 +0000

    done with scope level and mini hositing

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,5 +12,19 @@ if(true){
 // console.log(a); 
 // console.log(b);
 // c value gets printed
-console.log(c); // 30 is getting printed instead of 300, therefore var is not used much.
+// console.log(c); // 30 is getting printed instead of 300, therefore var is not used much.
                 //  It becomes hectic to solve errors when lot of developers are working with code.
+
+
+// ************************* Mini hoisting******************************
+
+addOne(5) 
+function addOne(num){ // this type of function declare can run this code
+    return num+1 
+}
+
+
+// addTwo(6) 
+const addTwo = function(num){ //but this type cannot (function where ur holding it in variable)
+    return num+2
+}
