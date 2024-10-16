976c4f8fd96d67eddb04c307bad5b6e345f8cc8f
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-10-16 11:22:18 +0000

    done with high oder array loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set("IN", "India")
+map.set("USA", "United States of America")
+map.set("FR", "France")
+
+// console.log(map);
+
+for (let [key, value] of map){
+    // console.log(key, ':-', value);
+    
+}
+
+// trying to iterate objects in similar way
+
+myObject = {
+    "game1": "NFS",
+    "game2": "Spiderman"
+}
+
+for (const [key,value] of myObject){
+    console.log(key, ':-', value);
+}
+
+
