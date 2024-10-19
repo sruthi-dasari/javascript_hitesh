a3098e1056f0a35a810fda194957980c763d67c4
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-10-19 08:41:20 +0000

    done with for in and for each loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple",
+}
+
+for (const key in myObject) {
+   console.log(`${key} is the shortcut of ${myObject[key]}`);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key); // nothing gets printed since map is not iterable
+// }
