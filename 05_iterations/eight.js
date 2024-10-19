997a3da1c741aaacba023b2a4795f2d7d56309d9
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2024-10-19 16:51:26 +0000

    done with reduce and started dom

---
+++ 05_iterations/eight.js
@@ -0,0 +1,35 @@
+//reduce 
+
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function(acc, currval){
+//     // console.log(`acc is ${acc} and current val is ${currval}`);
+//     return (acc + currval)
+// }, 0)
+
+// const myTotal = myNums.reduce((acc, curr) => (acc + curr), 0)
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, curr) => acc + curr.price, 0)
+
+console.log(priceToPay);
---
+++ 05_iterations/seven.js
@@ -0,0 +1,16 @@
+// map
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.map((num) => num + 10) // map returns an array (foreach cannot return)
+
+// console.log(newNums);
+
+// chaining
+
+const newNums = myNums
+.map((num) => num * 10)
+.map((num) => num + 1)
+.filter((num) => num >= 40)
+
+console.log(newNums);
